84993cfb63c62820ef55fd79a6a35f5ec948af12
DEMEOTHEDEVELOPER <99264890+DEMEOTHEDEVELOPER@users.noreply.github.com>
2025-01-02 16:31:34 -0500

    Made progress on GUI

--- src/SimplyStocksApp.java
+++ src/SimplyStocksApp.java
@@ -1,11 +1,25 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class SimplyStocksApp extends Application{
+
     public void start(Stage primaryStage) {
         Pane pane = new Pane();
+
+        SimplyStocksAppView view = new SimplyStocksAppView();
+        pane.getChildren().add(view);
+
+        view.getAccountsPane().getAddAccountBtn().setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent actionEvent) {
+                
+            }
+        });
+
         Scene scene = new Scene( pane);
         primaryStage.setTitle("Simply Stocks");
         primaryStage.setScene(scene);
--- src/SimplyStocksAppView.java
+++ src/SimplyStocksAppView.java
@@ -1,3 +1,17 @@
-public class SimplyStocksAppView {
+import javafx.scene.layout.Pane;
 
+public class SimplyStocksAppView extends Pane {
+    private AccountsPane accountsPane;
+
+    public AccountsPane getAccountsPane() {return accountsPane;}
+
+    public SimplyStocksAppView() {
+        accountsPane = new AccountsPane();
+        accountsPane.relocate(0,0);
+        accountsPane.setPrefSize(250, 500);
+
+        getChildren().addAll(accountsPane);
+
+        setPrefSize(1000, 500);
+    }
 }
